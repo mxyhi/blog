f3c4f7cf21e637b2fed23c214115113e6074a9ea
mxyhi <2528988582@qq.com>
2023-12-15 14:12:28 +0800

    🔧

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://mxyhi.github.io',
+	base: '/blog',
 	integrations: [
 		starlight({
 			title: 'My Docs',
